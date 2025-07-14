5870578b234c8a54d0d3cb4ca9543b2fd5c974dc
gaurav-tikhile-CG <gaurav.kishor-tikhile@capgemini.com>
2025-07-14 11:25:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
-
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-demo-bucket-123456"
-  acl    = "private"
+  bucket = "my-terraform-demo-bucket-${random_id.bucket_id.hex}"
+  acl    = "private"
 }
-  
+
