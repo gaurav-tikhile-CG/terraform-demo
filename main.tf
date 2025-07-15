ee71222c9c70bc85853bcb20440769c6ed85fb2b
gaurav-tikhile-CG <gaurav.kishor-tikhile@capgemini.com>
2025-07-15 11:41:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 data "aws_iam_users" "all" {}
 
 output "iam_user_names" {
   value = data.aws_iam_users.all.names
-}
-
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
 }
