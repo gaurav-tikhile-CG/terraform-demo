2a27680f05fe07c113a2670c569187a770b946e1
gaurav-tikhile-CG <gaurav.kishor-tikhile@capgemini.com>
2025-07-17 11:00:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,7 @@
 variable "region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources"
   type        = string
 }
+
 
 //This allows Terraform to recognize var.aws_region as a valid input variable.
